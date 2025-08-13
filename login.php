a6dcb7cb5d203a014f78322dad53d701c3a20eae
cccat970122 <gary8662009@gmail.com>
2025-08-13 18:12:18 +0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,16 +1,23 @@
 <?php
-$correct_username = "admin";
-$correct_password = "password123";
+header('Content-Type: text/html; charset=utf-8');
+
+$users = [
+    'admin' => 'password123',
+    'root'  => 'supersecretpassword' // 這個帳號的密碼你不需要給使用者
+];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    if ($username === $correct_username && $password === $correct_password) {
-        echo "<h1>登入成功！</h1>";
-        echo "<p>恭喜你！這是你的旗標：<b>flag{easy_brute_force}</b></p>";
+
+    if (isset($users[$username]) && $users[$username] === $password) {
+        // 成功登入
+        // 使用 Base64 編碼使用者名稱，並設定為 Cookie
+        setcookie('sessionid', base64_encode($username), time() + 3600, "/");
+        header("Location: member.php"); // 導向會員頁面
+        exit();
     } else {
-        echo "<h1>登入失敗！</h1>";
-        echo "<p>帳號或密碼錯誤，請再試一次。</p>";
+        echo "<h1>登入失敗！</h1><p>帳號或密碼錯誤，請再試一次。</p>";
     }
 }
-
 ?>
